60ea9845f30c7c2ad6820a9cb3a5792bd3552199
Otávio Augusto Cartaxo Araújo <otavioau.cartaxo@gmail.com>
2017-08-21 19:22:47 -0300

    structs.h -> DLL.h

---
+++ DLL.h
@@ -0,0 +1,42 @@
+#ifndef _STCT_H_
+#define _STCT_H_
+
+
+typedef struct informacoes{
+
+    int matricula;
+    char* nome;
+    char* sobrenome;
+    char* email;
+    char* telefone;
+    float salario;
+
+}INFO;
+
+
+typedef struct lista {
+
+	INFO pessoa;
+	
+	struct lista* proximo;
+	struct lista* anterior;
+
+
+}LISTA;
+
+
+void importaRegistro(LISTA** headRef, char arquivo[]);
+void exibeRegistro(LISTA* node);
+void deletaMatricula(LISTA** headRef);
+void buscaPelaMatricula(LISTA* node);
+void buscaPeloNome(LISTA* node);
+void insereOrdenado(LISTA** headRef);
+LISTA* criaNovoNo();
+
+void push(LISTA** headRef, LISTA* newNode);
+void insertAfter(LISTA* prevNode, LISTA* newNode);
+void deleteNode(LISTA** headRef, LISTA* del);
+void append(LISTA** headRef, LISTA* newNode);
+void printList(LISTA* node);
+
+#endif
